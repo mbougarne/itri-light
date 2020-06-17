6364364538330b7fe60b013bc34b25005b3a7c4c
Mourad Bougarne <contact@mbougarne.me>
2020-06-17 19:37:42 +0100

    Init the app, add user and profile

---
+++ app/Models/Profile.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * Trim and convert the first letter to upper case
+     *
+     * @param string $value
+     * @return void
+     */
+    public function setFirstNameAttribute($value)
+    {
+        $this->attributes['first_name'] = trim(ucfirst($value));
+    }
+
+    /**
+     * Trim and convert the last letter to upper case
+     *
+     * @param string $value
+     * @return void
+     */
+    public function setLastNameAttribute($value)
+    {
+        $this->attributes['last_name'] = trim(strtolower($value));
+    }
+
+    /**
+     * Get the first name in title case
+     *
+     * @param string $value
+     * @return string
+     */
+    public function getFirstNameAttribute($value)
+    {
+        return ucfirst($value);
+    }
+
+    /**
+     * Get the last name in title case
+     *
+     * @param string $value
+     * @return string
+     */
+    public function getLastNameAttribute($value)
+    {
+        return ucfirst($value);
+    }
+
+    /**
+     * Get the last name in title case
+     *
+     * @param string $value
+     * @return string
+     */
+    public function getAvatarAttribute($value)
+    {
+        return ($value) ? 'avatars/' . $value : 'img/default-avatar.png';
+    }
+
+    /**
+     * Profile owner
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ app/Models/User.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * Trim username space before saving
+     *
+     * @param string $value
+     * @return void
+     */
+    public function setUsernameAttribute($value)
+    {
+        $this->attributes['username'] = trim($value);
+    }
+
+    /**
+     * User profile
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function profile()
+    {
+        return $this->hasOne(Profile::class);
+    }
+}
