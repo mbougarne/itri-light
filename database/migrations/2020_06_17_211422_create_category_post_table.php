c059f64a576406a410ee788154f163b0c3aefa99
Mourad Bougarne <contact@mbougarne.me>
2020-06-17 22:17:27 +0100

    Add category_post pivot table

---
+++ database/migrations/2020_06_17_211422_create_category_post_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoryPostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category_post', function (Blueprint $table) {
+            $table->primary(['category_id', 'post_id']);
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->unsignedBigInteger('post_id')->nullable();
+
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category_post');
+    }
+}
