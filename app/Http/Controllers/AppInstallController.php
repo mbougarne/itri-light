2eb7ebd6b753ea05ed88c791016f18b821763518
Mourad Bougarne <contact@mbougarne.me>
2020-06-18 20:15:31 +0100

    Add app install controller

---
+++ app/Http/Controllers/AppInstallController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\InstallApplication;
+
+class AppInstallController
+{
+    /**
+     * Install the application
+     *
+     * @param \App\Http\Requests\InstallApplication $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(InstallApplication $request)
+    {
+
+    }
+
+}
