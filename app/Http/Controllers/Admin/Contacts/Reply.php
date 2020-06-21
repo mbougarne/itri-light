184d9884bd1efeef91316d5d3aa936377da182c3
Mourad Bougarne <contact@mbougarne.me>
2020-06-21 01:45:58 +0100

    Add reply controller

---
+++ app/Http/Controllers/Admin/Contacts/Reply.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Contacts;
+
+use App\Models\Contact;
+
+class Reply
+{
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Contact  $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Contact $contact)
+    {
+
+    }
+}
